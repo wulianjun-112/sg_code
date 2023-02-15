bf3222a81158dea901555668810562d7dabdb252
wulianjun-112 <1204457392@qq.com>
2023-02-15 18:46:48 +0800

    a

--- tools/start.sh
+++ tools/start.sh
@@ -5,5 +5,5 @@ export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 mkdir /output_dir/
 chmod 777 tools/start.sh
-python -u tools/test_docker.py > ./output_dir/test.log
+python -u tools/test_docker.py > /output_dir/test.log
 /bin/bash
